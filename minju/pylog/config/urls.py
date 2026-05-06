a225d3512dc22ca449e26dc1357cdc10a9ad80eb
llszos <llszos@naver.com>
2026-05-07 04:18:09 +0900

    pylog ch 14 코드 실습

--- minju/pylog/config/urls.py
+++ minju/pylog/config/urls.py
@@ -22,13 +22,14 @@ from django.contrib import admin
 from django.urls import path
 
 from config.views import index
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_add
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('posts/', post_list),
     path('posts/<int:post_id>/', post_detail),
+    path("posts/add/", post_add),
 ]
 
 urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
