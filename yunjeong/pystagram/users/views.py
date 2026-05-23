c82cfd7a08a700dd9f2cffdfffd3143ab5970680
dw261 <woduuc099@gmail.com>
2026-05-23 13:40:37 +0900

    ch 21, 22, 23 실습코드

--- yunjeong/pystagram/users/views.py
+++ yunjeong/pystagram/users/views.py
@@ -1,7 +1,7 @@
-from lib2to3.fixes.fix_input import context
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from users.forms import LoginForm, SignupForm
 from users.models import User
 
@@ -66,3 +66,47 @@ def signup(request):
     # 2. GET 요청으로 비 form이 생성된 경우 -> 빈 form이 사용자에게 보여진다.
     context = {"form": form}
     return render(request, 'users/signup.html', context)
+
+def profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    context = {
+        "user": user
+    }
+    return render(request, "users/profile.html", context)
+
+def followers(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    relationships = user.follower_relationships.all()
+    context = {
+        "user": user,
+        "relationships": relationships,
+    }
+    return render(request, "users/followers.html", context)
+
+def following(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    relationships = user.following_relationships.all()
+    context = {
+        "user": user,
+        "relationships": relationships,
+    }
+    return render(request, "users/following.html", context)
+
+def follow(request, user_id):
+    # 로그인한 유저
+    user = request.user
+    # 팔로우하려는 유저
+    target_user = get_object_or_404(User, id=user_id)
+
+    # 팔로우하려는 유저가 이미 자신의 팔로잉 목록에 있는 경우
+    if target_user in user.following.all():
+        # 팔로잉 목록에서 제거
+        user.following.remove(target_user)
+    # 팔로우 하려는 유저가 자신의 팔로잉 목록에 없는 겨우
+    else:
+        user.following.add(target_user)
+
+    # 팔로우 토글 후 이동할 URL이 전달되었다면 해당 주소로,
+    # 전달되지 않았다면 로그인 한 유저의 프로필 페이지로 이동
+    url_next = request.GET.get("next") or reverse("users:profile", args=[user_id])
+    return HttpResponseRedirect(url_next)
